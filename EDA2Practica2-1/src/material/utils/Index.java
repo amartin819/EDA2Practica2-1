856636d3821e11c710d6bb52421c9d711fd12816
amart <amart@192.168.1.43>
2016-12-20 14:57:31 +0100

    solo leer fichero

--- EDA2Practica2-1/src/material/utils/Index.java
+++ EDA2Practica2-1/src/material/utils/Index.java
@@ -1,5 +1,12 @@
 package material.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import material.ordereddictionary.BSTOrderedDict;
+import material.ordereddictionary.Entry;
+
+
 /**
  *
  * @author jvelez
@@ -11,8 +18,44 @@ public class Index {
      * @param filename Name of the file.
      * @param stopwords Name of the stopwords file.
      */
-    public void load(String filename, String stopwords) {
-        throw new RuntimeException("Implements this method.");
+    
+    BSTOrderedDict<String, Integer> ejemplo = new BSTOrderedDict<>();
+    BSTOrderedDict<String, Integer> stop = new BSTOrderedDict<>();
+    
+    public void load(String filename, String stopwords) throws FileNotFoundException {
+        //leer el fichero de palabras prohibidas
+        Scanner scStop= new Scanner(new File(stopwords));
+          
+        Entry<String, Integer> aux;
+        
+        while(scStop.hasNextLine()){
+            this.stop.insert(scStop.next(), Integer.SIZE);
+            System.out.println(scStop.nextLine());
+        }
+        
+         
+        //leer el fichero del cuento
+        Scanner scName= new Scanner(new File(filename));
+         
+        while(scName.hasNextLine()){
+            aux = stop.find(scName.next());
+            if(aux == null)
+            //if(Character.isUpperCase(scName.next()) && !scName.hasNextInt()){
+            this.ejemplo.insert(scName.next(), Integer.SIZE);
+            System.out.println(scName.nextLine());
+        }
+        scStop.close();
+        scStop.close(); 
+       /*  while((bfNameRead = bfName.readLine()) != null){
+             tempName = tempName + bfNameRead;
+            if(Character.isUpperCase(tempName.charAt(0)) && !Character.isDigit(tempName.charAt(0))){
+                    aux= stop.find(tempName);
+                    if(aux==null){
+                        ejemplo.insert(tempName, Integer.SIZE);
+                    }
+            }
+        }*/
+       
     }
 
     /**
@@ -21,7 +64,13 @@ public class Index {
      * in the text at its pages out of the stopword list.
      */
     public String allOccurrencesIndex() {
-        throw new RuntimeException("Implements this method.");
+        String devolver = "";
+
+        while(!this.ejemplo.isEmpty()){
+           
+        }
+        
+        return devolver;
     }
 
     /**
@@ -31,5 +80,10 @@ public class Index {
      */
     public String firstOccurrencesIndex() {
         throw new RuntimeException("Implements this method.");
+    }
+    
+    
+    public static void main(String[] args){
+        load("ejemplo.txt", "stopwords.txt");
     }
 }
